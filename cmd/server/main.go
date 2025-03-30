11aca849add2e915db82c08e51b605de72fb0642
Midiy <polyk6859@gmail.com>
2025-03-30 15:47:53 +0300

    install server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,34 @@
-package server
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/Glack134/pc_club/pkg/rpc"
+	"google.golang.org/grpc"
+)
+
+type adminServer struct {
+	pb.UnimplementedAdminServiceServer
+}
+
+func (s *adminServer) GrantAccess(ctx context.Context, req *pb.GrantRequest) (*pb.Response, error) {
+	log.Printf("Access granted to %s on PC %s for %d minutes", req.UserId, req.PcId, req.Minutes)
+	return &pb.Response{Success: true, Message: "Access granted!"}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterAdminServiceServer(s, &adminServer{})
+
+	log.Println("Server running on :50051")
+	if err := s.Serve(lis); err != nil {
+		log.Fatal("Failed toserve: %v", err)
+	}
+}
