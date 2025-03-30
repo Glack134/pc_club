883b0a0a2c7eec39cfa1371e97ea79ab3570c178
Midiy <polyk6859@gmail.com>
2025-03-30 15:55:03 +0300

    first start client and server

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/Glack134/pc_club/pkg/rpc"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewAdminServiceClient(conn)
+
+	resp, err := client.GrantAccess(context.Background(), &pb.GrantRequest{
+		UserId:  "brother",
+		PcId:    "gaming-pc-1",
+		Minutes: 60,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Server response: %v", resp.Message)
+}
